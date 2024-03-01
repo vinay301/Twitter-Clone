b0b4913477721e4ba270084292e20c785816cb69
vinay <vinaysharma.vs743@gmail.com>
2024-03-01 21:49:45 +0530

    S3 Bucket for Image Storage

--- twitter-server/src/app/tweets/resolvers.ts
+++ twitter-server/src/app/tweets/resolvers.ts
@@ -1,14 +1,36 @@
 import { Tweet } from "@prisma/client";
 import { prismaClient } from "../../clients/database";
 import { GraphQLContext } from "../../interfaces";
+import { S3Client,PutObjectCommand} from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import UserService from "../../services/user";
+import TweetService, { CreateTweetPayload } from "../../services/tweet";
 
-interface CreateTweetPayload{
-    content: string
-    imageUrl?: string
-}
+
+const s3Client = new S3Client({
+    region:process.env.AWS_DEFAULT_REGION,
+})
 
 const queries = {
-    getAllTweets: () => prismaClient.tweet.findMany({orderBy: {createdAt:'desc'}}),
+    getAllTweets: () => TweetService.getAllTweets(),
+    getSignedURLForTweet: async(parent:any, {imageType, imageName}:{imageType:string, imageName:string}, context:GraphQLContext) => {
+        if(!context.user || !context.user.id)
+        {
+            throw new Error ("You must be logged in to perform this action.");
+        }
+        try{
+            const allowedImageTypes = ['image/jpg','image/jpeg','image/png','image/webp'];
+            if(!allowedImageTypes.includes(imageType)) throw new Error("Unsupported image type");
+            const putObjectCommand = new PutObjectCommand({
+                Bucket : process.env.AWS_S3_BUCKET,
+                Key: `uploads/${context.user.id}/tweets/${imageName}-${Date.now().toString()}.${imageType}`,
+            })
+            const signedUrl = await getSignedUrl(s3Client,putObjectCommand)
+            return signedUrl;
+        }catch(error){
+
+        }
+    }
 }
 
 const mutations = {
@@ -19,14 +41,10 @@ const mutations = {
         }
 
         try{
-            const tweet = await prismaClient.tweet.create({
-                data:{
-                    content: payload.content,
-                    imageUrl: payload.imageUrl || null,
-                    author: {connect: {id:context.user.id}}
-                    // authorId: context.user.id
-                }
-            })
+            const tweet = await TweetService.createTweet({
+                ...payload,
+                userId: context.user.id
+            });
             return tweet;
         }catch(error){
             console.log(error);
@@ -38,7 +56,7 @@ const mutations = {
 
 const extraResolvers = {
     Tweet: {
-        author: (parent:Tweet) => prismaClient.user.findUnique({where:{id:parent.authorId}})
+        author: (parent:Tweet) =>UserService.getUserById(parent.authorId),
     }
 }
 
---
+++ twitter-server/src/services/tweet.ts
@@ -0,0 +1,25 @@
+import { prismaClient } from "../clients/database";
+
+export interface CreateTweetPayload{
+    content: string
+    imageUrl?: string
+    userId: string
+}
+
+class TweetService{
+    public static createTweet(data:CreateTweetPayload){
+        return prismaClient.tweet.create({
+            data:{
+                content: data.content,
+                imageUrl: data.imageUrl || null,
+                author: {connect: {id:data.userId}}
+            }
+        })
+    }
+
+    public static getAllTweets(){
+        return prismaClient.tweet.findMany({orderBy: {createdAt:'desc'}})
+    }
+}
+
+export default TweetService;
